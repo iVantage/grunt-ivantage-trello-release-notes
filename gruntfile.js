36b67420e43a507aa05d7b28756dca9f74f074d2
Justin <jus.russell@gmail.com>
2015-06-03 12:32:51 -0400

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,66 @@
+/*
+ * grunt-ivantage-trello-release-notes
+ * https://github.com/ivantage/grunt-ivantage-trello-release-notes
+ *
+ * Copyright (c) 2015 jtrussell
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    ivantage_trello_release_notes: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      }
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'ivantage_trello_release_notes', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/ivantage_trello_release_notes.js
@@ -0,0 +1,94 @@
+
+/*
+ * grunt-ivantage-trello-release-notes
+ * https://github.com/ivantage/grunt-ivantage-trello-release-notes
+ *
+ * Copyright (c) 2015 iVantage
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  var Trello = require('node-trello')
+    , trelloUtils = require('node-trello-utils')
+    , async = require('async')
+    , hbs = require('handlebars');
+
+  grunt.registerMultiTask('ivantage_trello_release_notes', 'Build release notes from user stories in Trello cards.', function() {
+    var done = this.async();
+
+    // Try to gather some info on the host package for more useful defaults
+    var pkg;
+    try {
+      pkg = grunt.file.readJSON('package.json');
+    } catch(err) {
+      pkg = {version: 'X.Y.Z'};
+    }
+
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var opts = this.options({
+      sprintBoards: 'baord1,board2',
+      productLabel: 'awesomeProduct',
+      featuresLabel: 'Feature',
+      bugLabel: 'Bug',
+      outfile: 'releasenotes/tag-v' +  + '.md',
+      trelloApiKey: '',
+      trelloToken: '',
+      doneListName: 'Live/Done',
+      headerTpl: '### Version ' + pkg.version,
+      storyTpl: '- {{name}} ([go to card]({{url}}))'
+    });
+
+    console.log(grunt.file.readJSON('package.json'));
+
+    var t = new Trello(opts.trelloApiKey, opts.trelloToken);
+
+    var sprintBoardIds = opts
+        .sprintBoards
+        .split(',')
+        .map(trelloUtils.getBoardIdFromUrl);
+
+    var productLabel = opts.productLabel.toLowerCase();
+
+    var tpl = hbs.compile(opts.storyTpl);
+
+    async.map(
+      sprintBoardIds,
+      function(boardId, cb) {
+        trelloUtils.getCardsByListName(t, boardId, opts.doneListName, true, cb);
+      },
+      function(err, cardsAndCards) {
+        if(err) {
+          grunt.log.error(err);
+          return done(false);
+        }
+        var cards = Array.prototype.concat.apply([], cardsAndCards);
+        
+        // We only want the cards tagged with the given product label
+        cards = cards.filter(function(c) {
+          return c.labels.filter(function(f) {
+            return f.name.toLowerCase() === productLabel;
+          }).length > 0;
+        });
+
+        buildReleaseNotesFromCards(cards);
+      });
+
+    var buildReleaseNotesFromCards = function(cards) {
+      var notes = cards.map(function(c) {
+        return tpl(c);
+      });
+
+      grunt.file.write(opts.outfile, [opts.headerTpl, ''].concat(notes).join('\n'));
+      grunt.log.writeln('Release note written to ' + opts.outfile);
+    };
+
+  });
+
+};
