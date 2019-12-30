7367b71c14de54bcdd0daf72933f85c7d98c6677
Justin Russell <jus.russell@gmail.com>
2019-12-30 15:07:28 -0500

    fix: Update deps and remove grunt-jscs
    The most recent version of grunt-jscs relies on outdated packages with
    security vulnerabilities.

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,3 @@
-
 /*
  * grunt-ivantage-trello-release-notes
  * https://github.com/ivantage/grunt-ivantage-trello-release-notes
@@ -7,35 +6,17 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-module.exports = function(grunt) {
-
-  var pkg = grunt.file.readJSON('package.json');
+module.exports = function (grunt) {
+  var pkg = grunt.file.readJSON('package.json')
 
   // Project configuration.
   grunt.initConfig({
     jshint: {
-      all: [
-        'gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
+      all: ['gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'],
       options: {
         jshintrc: '.jshintrc'
-      }
-    },
-
-    jscs: {
-      options: {
-        config: '.jscsrc'
-      },
-      files: {
-        src: [
-        'gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-        ]
       }
     },
 
@@ -81,16 +62,15 @@ module.exports = function(grunt) {
       },
       tests: ['test/*_test.js']
     }
-  });
+  })
 
   // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks')
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscs');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-nodeunit')
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
@@ -99,9 +79,8 @@ module.exports = function(grunt) {
     //'ivantage_trello_release_notes',
     'ivantage_trello_release_notes_publish',
     'nodeunit'
-  ]);
+  ])
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'jscs', 'test']);
-
-};
+  grunt.registerTask('default', ['jshint', 'test'])
+}
